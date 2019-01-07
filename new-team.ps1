bcc22f92b762d1a4eff41855e015359dd4231d7d
StevenC365 <46461274+StevenC365@users.noreply.github.com>
2019-01-07 21:08:17 +0000

    Add files via upload

---
+++ new-team.ps1
@@ -0,0 +1,20 @@
+﻿Param(
+
+    [string]$teamname, #The name of the Team
+    [string]$teamowner, #The email address of the owner of the Team
+    [string]$teamalias #The bit of the teamname you want to be the URL
+
+)
+
+#Connect to Teams PowerShell
+$cred = Get-AutomationPSCredential -Name "TeamsAdmin"
+$teams = Connect-MicrosoftTeams -Credential $cred
+
+#Create the Team
+$newteam = New-Team -Alias $teamalias -DisplayName $teamname -Owner $teamowner -AccessType Private 
+
+#Return to Flow the new teams GroupID by passing the object as JSON
+write-output $newteam | convertto-json
+
+#Hangup
+Disconnect-MicrosoftTeams
